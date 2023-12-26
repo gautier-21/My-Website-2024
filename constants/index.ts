aa148be58d13636245eb81994111429a78706795
Gautier M <hello.gautiermaire@gmail.com>
2023-12-26 18:46:35 +0100

    Init

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Gautier Maire'
+export const appDescription = 'My personal website.'
